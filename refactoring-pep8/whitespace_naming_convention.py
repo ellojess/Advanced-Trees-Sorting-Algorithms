f8fbbe14fed37d10244c644588e88cb928722249
Jess <jtrinh619@gmail.com>
2021-02-02 10:26:54 -0800

    [complete] refactoring-pep8 activities

--- refactoring-pep8/whitespace_naming_convention.py
+++ refactoring-pep8/whitespace_naming_convention.py
@@ -1,17 +1,20 @@
 # PEP8 - whitespaces and variable names.
 # This is a guess game. Guess what number the computer has chosen!
 import random
-lowerLimit=0
-UPPER_limit= 100
-randomNumber = random.randint (lowerLimit ,UPPER_limit )
-print ('The computer has selected a number between 0 and 100. Use your supernatural superpowers to guess what the number is.')
+
+lower_limit=0
+upper_limit= 100
+random_number = random.randint (lower_limit ,upper_limit )
+print ('The computer has selected a number between 0 and 100. \
+    Use your supernatural superpowers to guess what the number is.')
+
 while True:
-    UserGuess = int (input("Enter a number between 0 and 100 (including): "))
-    if UserGuess> randomNumber:
+    user_guess = int(input("Enter a number between 0 and 100 (including): "))
+    if user_guess> random_number:
         print ("Guess a smaller number.")
-    elif UserGuess <randomNumber:
+    elif user_guess < random_number:
         print ("Guess a larger number.")
-    else: #UserGuess == randomNumber:
+    else: # user_guess == random_number:
         print ("You Won!")
         break
     
